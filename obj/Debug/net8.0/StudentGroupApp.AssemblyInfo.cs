7ac8ec75492d4a3b437c8f2853b2ef75fc349b15
arturdancuk5 <arturdancuk5@gmail.com>
2026-06-12 04:52:54 +0300

    feat: ініціалізація Практичної роботи №8

--- obj/Debug/net8.0/StudentGroupApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentGroupApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentGroupApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dbce65c5eb6857d68b3a166c9899aef2c5a9f07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c6127dc525c7546171b3c609d443f894f40b3fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentGroupApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentGroupApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
